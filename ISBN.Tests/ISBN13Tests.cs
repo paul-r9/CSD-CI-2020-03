45c09aa81ff88b502b9b06bd6685ebf8e612b5f0
Robert Merino <rmerino@autocrib.com>
2020-03-06 15:04:20 -0800

    add ValidInputLength

--- ISBN.Tests/ISBN13Tests.cs
+++ ISBN.Tests/ISBN13Tests.cs
@@ -19,6 +19,32 @@ namespace ISBN
             //Assert
             Assert.True(sut.calcCheckSum13(goodISBN));
         }
+        [Fact]
+        public void validInputLength13()
+        {
+            //Arrange
+            string goodISBN = "9780131177055";
 
+            //Act
+            ISBNFinder sut = new ISBNFinder();
+            //BookInfo actual = sut.lookup(goodISBN);
+
+            //Assert
+            Assert.True(sut.ValidInputLength(goodISBN));
+        }
+
+        [Fact]
+        public void validInputLength10()
+        {
+            //Arrange
+            string goodISBN = "0134757599";
+
+            //Act
+            ISBNFinder sut = new ISBNFinder();
+            //BookInfo actual = sut.lookup(goodISBN);
+
+            //Assert
+            Assert.True(sut.ValidInputLength(goodISBN));
+        }
     }
 }
